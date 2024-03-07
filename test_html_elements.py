59eb964fd2415a1bdf76361c10c1e5e98273ae38
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2024-03-07 13:17:45 -0500

    Create test_html_elements.py

---
+++ test_html_elements.py
@@ -0,0 +1,29 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+class HtmlElementsTestCase(unittest.TestCase):
+    def setUp(self):
+        # Options for running Chrome headless
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        # Set path to your WebDriver here
+        self.driver = webdriver.Chrome(executable_path="/path/to/chromedriver", options=options)
+    def test_html_elements(self):
+        driver = self.driver
+        driver.get("http://yourwebsite.com")                                                                 #change this to your IP
+        # Check for the presence of a specific HTML element by ID
+        some_element = driver.find_element(By.ID, "someElementId")
+        self.assertIsNotNone(some_element)
+        # Check for the text in a specific element
+        heading = driver.find_element(By.TAG_NAME, "h1").text
+        self.assertEqual("Expected H1 Text", heading)
+        # Assert the presence of a class on a div element
+        special_div = driver.find_element(By.CLASS_NAME, "special-class")
+        self.assertIsNotNone(special_div)
+        # Assert the value of an attribute
+        image = driver.find_element(By.TAG_NAME, "img")
+        self.assertEqual("expected-image.jpg", image.get_attribute("src"))
+    def tearDown(self):
+        self.driver.quit()
+if __name__ == "__main__":
+    unittest.main()
